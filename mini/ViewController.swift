7e29388fb9e10c149625fd5b394ace1b4a2f3247
yelhsa06 <ayeung2164@gmail.com>
2022-06-28 12:30:38 -0400

    Initial Commit

---
+++ mini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mini
+//
+//  Created by Ashley Yeung on 6/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
